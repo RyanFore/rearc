8cd3d8f25aa708491d66d46dc3aa2ad984e2c733
Ryan Fore <fore.ryan@gmail.com>
2022-12-20 00:22:42 -0500

    Small cleanup

--- lambda_scraper/create_scraper_image.sh
+++ lambda_scraper/create_scraper_image.sh
@@ -2,12 +2,8 @@ id=$(aws sts get-caller-identity --query "Account" --output text)
 region=$(eval "aws configure get region")
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${id}.dkr.ecr.${region}.amazonaws.com
 
-docker build -t scraper_image . # --no-cache
+docker build -t scraper_image .
 docker tag scraper_image ${id}.dkr.ecr.${region}.amazonaws.com/rearc_scraper
 docker push ${id}.dkr.ecr.${region}.amazonaws.com/rearc_scraper
 
 aws lambda update-function-code --function-name rearc_terraform_scraper --image-uri ${id}.dkr.ecr.${region}.amazonaws.com/rearc_scraper:latest
-# aws lambda update-function-code --function-name rearc_terraform_scraper --image-uri 385871096247.dkr.ecr.us-east-1.amazonaws.com/rearc_scraper:latest
-
-
-# docker exec -ti $(docker ps -q --filter "ancestor=scraper_image") /bin/bash
